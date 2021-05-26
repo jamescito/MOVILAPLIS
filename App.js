6298d8915c771c4146e6b4f1427dd1c1c0979d93
jamescito <jr76407900@gmail.com>
2021-05-26 12:15:01 -0600

    ..

---
+++ App.js
@@ -0,0 +1,21 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {Text,StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const App = () => {
+ 
+
+  return (
+    <>
+    <Text>Hola mundo</Text>
+    </>
+  )
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
